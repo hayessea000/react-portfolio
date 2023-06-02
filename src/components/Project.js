85c6418a8bb98996c687124aba8a67e3f422300b
Sean Hayes <hayessea000@outlook.com>
2023-06-01 21:50:27 -0500

    set up project map

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,23 @@
 
-function Project(props){
-    const projects= [
-        {title: "1", image: "pic 1", link: "link1", repo:"repo1"},
-        {title: "2", image: "pic 2", link: "link2", repo:"repo2"},
-        {title: "3", image: "pic 3", link: "link3", repo:"repo3"},
-        {title: "4", image: "pic 4", link: "link4", repo:"repo4"},
-        {title: "5", image: "pic 5", link: "link5", repo:"repo5"},
-        {title: "6", image: "pic 6", link: "link6", repo:"repo6"}
+function Project(props) {
+    const projects = [
+        { id: "1", title: "1", image: "pic 1", link: "link1", repo: "repo1" },
+        { id: "2",  title: "2", image: "pic 2", link: "link2", repo: "repo2" },
+        { id: "3",  title: "3", image: "pic 3", link: "link3", repo: "repo3" },
+        { id: "4",  title: "4", image: "pic 4", link: "link4", repo: "repo4" },
+        { id: "5",  title: "5", image: "pic 5", link: "link5", repo: "repo5" },
+        { id: "6",  title: "6", image: "pic 6", link: "link6", repo: "repo6" }
     ]
-    return(
+    return (
         <>
-            <p>project</p>
-            <p>project</p>
-            <p>project</p>
+            {projects.map(project => (
+                <>
+                    <p key={project.id}>{project.title}</p>
+                    <p key={project.id}>{project.image}</p>
+                    <p key={project.id}>{project.link}</p>
+                    <p key={project.id}>{project.repo}</p>
+                </>
+            ))}
         </>
     )
 }
