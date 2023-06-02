cbbceeb3404f9b3ca23db546ae2c4a3893fa3ff7
Sean Hayes <hayessea000@outlook.com>
2023-06-02 16:14:13 -0500

    added project data

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,12 @@
 
 function Project(props) {
     const projects = [
-        { id: "1", title: "1", image: "pic 1", link: "link1", repo: "repo1" },
-        { id: "2",  title: "2", image: "pic 2", link: "link2", repo: "repo2" },
-        { id: "3",  title: "3", image: "pic 3", link: "link3", repo: "repo3" },
-        { id: "4",  title: "4", image: "pic 4", link: "link4", repo: "repo4" },
-        { id: "5",  title: "5", image: "pic 5", link: "link5", repo: "repo5" },
-        { id: "6",  title: "6", image: "pic 6", link: "link6", repo: "repo6" }
+        { id: "1", title: "Discussion Board", image: "pic 1", link: "https://discussion-board-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/discussion-board" },
+        { id: "2",  title: "What To Eat", image: "pic 2", link: "https://hayessea000.github.io/what-to-eat", repo: "https://github.com/hayessea000/what-to-eat" },
+        { id: "3",  title: "Tech Blog", image: "pic 3", link: "https://tech-blog-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/tech-blog" },
+        { id: "4",  title: "Weather Forecast", image: "pic 4", link: "https://hayessea000.github.io/weather-forecast/", repo: "https://github.com/hayessea000/weather-forecast" },
+        { id: "5",  title: "J.A.T.E", image: "pic 5", link: "https://jate-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/JATE" },
+        { id: "6",  title: "Todo Calendar", image: "pic 6", link: "https://hayessea000.github.io/todo-calendar/", repo: "https://github.com/hayessea000/todo-calendar" }
     ]
     return (
         <>
@@ -14,8 +14,8 @@ function Project(props) {
                 <div key={project.title} style={{border: "1px solid #333"}}>
                     <p key={project.title}>{project.title}</p>
                     <p key={project.image}>{project.image}</p>
-                    <p key={project.link}>{project.link}</p>
-                    <p key={project.repo}>{project.repo}</p>
+                    <p key={project.link}><a href={project.link}>Deplyed App</a></p>
+                    <p key={project.repo}><a href={project.repo}>Repo</a></p>
                 </div>
             ))}
         </>
