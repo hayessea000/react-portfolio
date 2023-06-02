f3b91337e60766d20bff5a6c6d2afb0cef0005b6
Sean Hayes <hayessea000@outlook.com>
2023-06-01 21:41:08 -0500

    set up contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,19 @@
 
 function Contact(props){
 
+    const [contactData, setContactData] = useState({ name: "", email: "", message: "" })
+
+    const handleInputChange = (e) => setContactData({...contactData, [e.target.name]: e.target.value})
+
     return(
         <>
-            <p>call me</p>
+             <form>
+                <label>Contact me</label>
+                <input type="text" placeholder="Your name here" name="name" value={contactData.name} onChange={handleInputChange} />
+                <input type="text" placeholder="Your Email" name="email" value={contactData.email} onChange={handleInputChange} />
+                <input type="text" placeholder="Your message" name="message" value={contactData.message} onChange={handleInputChange} />
+                <button>Contact</button>
+            </form>
         </>
     )
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,13 @@
 
 function Project(props){
-
+    const projects= [
+        {title: "1", image: "pic 1", link: "link1", repo:"repo1"},
+        {title: "2", image: "pic 2", link: "link2", repo:"repo2"},
+        {title: "3", image: "pic 3", link: "link3", repo:"repo3"},
+        {title: "4", image: "pic 4", link: "link4", repo:"repo4"},
+        {title: "5", image: "pic 5", link: "link5", repo:"repo5"},
+        {title: "6", image: "pic 6", link: "link6", repo:"repo6"}
+    ]
     return(
         <>
             <p>project</p>
