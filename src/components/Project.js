c231998b22743755dfd845fbfbcfaaf4f7f4aaf6
Sean Hayes <hayessea000@outlook.com>
2023-06-02 19:15:35 -0500

    set up all images

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,20 +2,20 @@
 function Project(props) {
     const projects = [
         { id: "1", title: "Discussion Board", image: "discussionBoardImage.png", link: "https://discussion-board-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/discussion-board" },
-        { id: "2",  title: "What To Eat", image: "pic 2", link: "https://hayessea000.github.io/what-to-eat", repo: "https://github.com/hayessea000/what-to-eat" },
-        { id: "3",  title: "Tech Blog", image: "pic 3", link: "https://tech-blog-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/tech-blog" },
-        { id: "4",  title: "Weather Forecast", image: "pic 4", link: "https://hayessea000.github.io/weather-forecast/", repo: "https://github.com/hayessea000/weather-forecast" },
-        { id: "5",  title: "J.A.T.E", image: "pic 5", link: "https://jate-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/JATE" },
-        { id: "6",  title: "Todo Calendar", image: "pic 6", link: "https://hayessea000.github.io/todo-calendar/", repo: "https://github.com/hayessea000/todo-calendar" }
+        { id: "2",  title: "What To Eat", image: "whattoeat.png", link: "https://hayessea000.github.io/what-to-eat", repo: "https://github.com/hayessea000/what-to-eat" },
+        { id: "3",  title: "Tech Blog", image: "techblogimage.png", link: "https://tech-blog-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/tech-blog" },
+        { id: "4",  title: "Weather Forecast", image: "weather-forecast.png", link: "https://hayessea000.github.io/weather-forecast/", repo: "https://github.com/hayessea000/weather-forecast" },
+        { id: "5",  title: "J.A.T.E", image: "jateimage.png", link: "https://jate-hayessea000.herokuapp.com/", repo: "https://github.com/hayessea000/JATE" },
+        { id: "6",  title: "Todo Calendar", image: "todo-calendar.png", link: "https://hayessea000.github.io/todo-calendar/", repo: "https://github.com/hayessea000/todo-calendar" }
     ]
     return (
         <>
             {projects.map(project => (
                 <div key={project.title} style={{border: "1px solid #333"}}>
                     <p key={project.title}>{project.title}</p>
-                    <img key={project.image} src= {"./images/"+project.image}/>
+                    <img style={{width: "10%"}} key={project.image} src= {"./images/"+project.image} alt=""/>
                     <p key={project.link}><a href={project.link}>Deployed App</a></p>
-                    <p key={project.repo}><a href={project.repo}>Repo</a></p>
+                    <p key={project.repo}><a href={project.repo}>Repo Link</a></p>
                 </div>
             ))}
         </>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,6 +3,9 @@ function Resume(props){
 
     return(
         <>
+            <div>
+                <img src="./images/resumesample.png" alt="resume"/>
+            </div>
             <a href="./images/resume.pdf" download>resume here</a>
             <p>proficiencies</p>
         </>
